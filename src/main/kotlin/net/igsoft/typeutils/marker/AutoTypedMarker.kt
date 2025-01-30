d52f278e342bb34ee2466b8b43cce7270d2ef9ef
Marcin Kuszczak <aarti@interia.pl>
2025-01-30 11:35:50 +0100

    Ability to create easily marker with labels

--- src/main/kotlin/net/igsoft/typeutils/marker/AutoTypedMarker.kt
+++ src/main/kotlin/net/igsoft/typeutils/marker/AutoTypedMarker.kt
@@ -1,9 +1,14 @@
 package net.igsoft.typeutils.marker
 
 import net.igsoft.typeutils.generator.IntGenerator
+import kotlin.properties.ReadOnlyProperty
 
 @Suppress("unused")
-open class AutoTypedMarker<T> protected constructor(override val id: Any, override val clazz: Class<T>, override val label: String) :
+open class AutoTypedMarker<T> protected constructor(
+    override val id: Any,
+    override val clazz: Class<T>,
+    override val label: String
+) :
     DefaultTypedMarker<T>(id, clazz, label) {
 
     companion object {
@@ -23,6 +28,12 @@ open class AutoTypedMarker<T> protected constructor(override val id: Any, overri
             )
         }
 
-        inline fun <reified T> create(label: String?= null) = create(T::class.java, label)
+        inline fun <reified T> create(label: String? = null) = create(T::class.java, label)
+
+        inline fun <reified T> createWithLabel(): ReadOnlyProperty<Any?, TypedMarker<T>> {
+            return ReadOnlyProperty { _, property ->
+                create(T::class.java, property.name)
+            }
+        }
     }
 }
